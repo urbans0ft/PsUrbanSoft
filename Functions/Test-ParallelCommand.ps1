d1b95720e1997019074e75dbe29efd2fc1fc9b54
Nico V. Urbanczyk <nico@urbansoft.eu>
2025-11-06 23:52:20 +0100

    analyze parameter via pscustomobject

--- Functions/Test-ParallelCommand.ps1
+++ Functions/Test-ParallelCommand.ps1
@@ -1,12 +1,23 @@
 Write-Host "Importing Invoke-ParallelCommand and running examples..." -ForegroundColor Cyan
 . .\Functions\Invoke-ParallelCommand.ps1
-Write-Host "Invoke single command example:" -ForegroundColor Yellow
+
+Write-Host "Invoke single command example:" -ForegroundColor Cyan
 Invoke-ParallelCommand -Command 'ffmpeg' -ArgumentList @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-')
-Write-Host "Invoke pipeline example (1):" -ForegroundColor Yellow
+
+Write-Host "Invoke pipeline example (1):" -ForegroundColor Cyan
 , @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-') | Invoke-ParallelCommand -Command 'ffmpeg'
-Write-Host "Invoke pipeline example (2):" -ForegroundColor Yellow
+
+Write-Host "Invoke pipeline example (2):" -ForegroundColor Cyan
  @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-'), @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-') | Invoke-ParallelCommand -Command 'ffmpeg'
-Write-Host "Invoke mixed pipeline and ArgumentList example:" -ForegroundColor Yellow
+
+ Write-Host "Invoke mixed pipeline and ArgumentList example:" -ForegroundColor Cyan
 @('-i', 'audio.m4a'), @('-i', 'audio.m4a') | Invoke-ParallelCommand -Command 'ffmpeg' -ArgumentList  '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-'
-Write-Host "Invoke PSCustomObject example:" -ForegroundColor Yellow
-[PSCustomObject]@{Command = 'ffmpeg'; ArgumentList = @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-') } | Invoke-ParallelCommand
+
+Write-Host "Invoke PSCustomObject single example:" -ForegroundColor Cyan
+[PSCustomObject]@{Command = 'ffmpeg'; ArgumentList = @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-') } |
+Invoke-ParallelCommand
+
+Write-Host "Invoke PSCustomObject multiple example:" -ForegroundColor Cyan
+[PSCustomObject]@{Command = 'ffmpeg'; ArgumentList = @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-') },
+[PSCustomObject]@{Command = 'ffmpeg'; ArgumentList = @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-') } |
+Invoke-ParallelCommand
