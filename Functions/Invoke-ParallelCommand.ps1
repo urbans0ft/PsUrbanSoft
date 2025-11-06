736ffc8114c16d92a8f5cb3b8ae1feab13e6ab9e
Nico V. Urbanczyk <nico@urbansoft.eu>
2025-11-06 23:43:17 +0100

    analyze parameter via pscustomobject

--- Functions/Invoke-ParallelCommand.ps1
+++ Functions/Invoke-ParallelCommand.ps1
@@ -22,26 +22,42 @@ function Invoke-ParallelCommand {
     .EXAMPLE
         @('-i', 'audio.m4a'), @('-i', 'audio.m4a') | Invoke-ParallelCommand -Command 'ffmpeg' -ArgumentList  '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-'
 
+        .EXAMPLE
+        [PSCustomObject]@{Command = 'ffmpeg'; ArgumentList = @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-') } | Invoke-ParallelCommand
+
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true, Position = 0)]
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipelineByPropertyName)]
         [string]$Command,
         
         [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
         [string[]]$PipelineArguments,
         
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
         [string[]]$ArgumentList
     )
 
     begin {
 
+        Write-Host "`$PSBoundParameters.ContainsKey('Command')           = '$($PSBoundParameters.ContainsKey("Command"))'" -ForegroundColor Magenta
+        Write-Host "`$PSBoundParameters.ContainsKey('PipelineArguments') = '$($PSBoundParameters.ContainsKey("PipelineArguments"))'" -ForegroundColor Magenta
+        Write-Host "`$PSBoundParameters.ContainsKey('ArgumentList')      = '$($PSBoundParameters.ContainsKey("ArgumentList"))'" -ForegroundColor Magenta
         [Collections.ArrayList]$argumentLists = @()
 
     }
     
     process {
+
+        Write-Host "`$Command           = '$Command'" -ForegroundColor Yellow
+        Write-Host "`$PipelineArguments = '$PipelineArguments'" -ForegroundColor Yellow
+        Write-Host "`$ArgumentList      = '$ArgumentList'" -ForegroundColor Yellow
+        Write-Host "`$Command.GetType()           = '$($Command.GetType())'" -ForegroundColor Yellow
+        Write-Host "`$PipelineArguments.GetType() = '$($PipelineArguments.GetType())'" -ForegroundColor Yellow
+        Write-Host "`$ArgumentList.GetType()      = '$($ArgumentList.GetType())'" -ForegroundColor Yellow
+        Write-Host "`$Command -is [PSCustomObject]           '$($Command -is [PSCustomObject])'" -ForegroundColor Yellow
+        Write-Host "`$PipelineArguments -is [PSCustomObject] '$($PipelineArguments -is [PSCustomObject])'" -ForegroundColor Yellow
+        Write-Host "`$ArgumentList -is [PSCustomObject]      '$($ArgumentList -is [PSCustomObject])'" -ForegroundColor Yellow
 
         [void]$argumentLists.Add($PipelineArguments)
 
@@ -49,6 +65,7 @@ function Invoke-ParallelCommand {
 
     end {
 
+        return
         # Pipeline input: combine each pipeline item with ArgumentList
         $argumentLists | ForEach-Object {
             $pipelineArgs = $_
--- Functions/Test-ParallelCommand.ps1
+++ Functions/Test-ParallelCommand.ps1
@@ -7,4 +7,6 @@ Write-Host "Invoke pipeline example (1):" -ForegroundColor Yellow
 Write-Host "Invoke pipeline example (2):" -ForegroundColor Yellow
  @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-'), @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-') | Invoke-ParallelCommand -Command 'ffmpeg'
 Write-Host "Invoke mixed pipeline and ArgumentList example:" -ForegroundColor Yellow
- @('-i', 'audio.m4a'), @('-i', 'audio.m4a') | Invoke-ParallelCommand -Command 'ffmpeg' -ArgumentList  '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-'
+@('-i', 'audio.m4a'), @('-i', 'audio.m4a') | Invoke-ParallelCommand -Command 'ffmpeg' -ArgumentList  '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-'
+Write-Host "Invoke PSCustomObject example:" -ForegroundColor Yellow
+[PSCustomObject]@{Command = 'ffmpeg'; ArgumentList = @('-i', 'audio.m4a', '-vn', '-filter:a', 'loudnorm=I=-24:LRA=7:TP=-2:dual_mono=false:print_format=json', '-f', 'null', '-') } | Invoke-ParallelCommand
